1180b41bd96065bc2660838700123b959d3ad678
sergejsvoronins <svoronins@gmail.com>
2023-05-26 14:08:19 +0200

    db is updated

--- oop/bokrejset/db.sql
+++ oop/bokrejset/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 24 maj 2023 kl 13:28
+-- Tid vid skapande: 26 maj 2023 kl 14:07
 -- Serverversion: 10.4.28-MariaDB
 -- PHP-version: 8.2.4
 
@@ -38,7 +38,6 @@ CREATE TABLE `authors` (
 --
 
 INSERT INTO `authors` (`id`, `first_name`, `last_name`) VALUES
-(1, 'test_fn', 'test_lv'),
 (5, 'Emely', 'Henry'),
 (6, 'Prince', 'Harry'),
 (7, 'Lucy', 'Score'),
@@ -64,8 +63,7 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`id`, `title`, `year`, `author_id`) VALUES
-(1, 'The great book', 2023, 1),
-(2, 'test', 2010, 1),
+(1, 'The great book', 2023, 5),
 (3, 'Happy Place', 2023, 5),
 (4, 'Spare', 2023, 6),
 (5, 'Things We Hide from the Light', 2023, 7),
@@ -97,7 +95,8 @@ INSERT INTO `userbook` (`id`, `user_id`, `book_id`, `pages`, `comment`) VALUES
 (7, 4, 3, 504, 'Spännande boken   '),
 (8, 5, 7, 398, 'Kan varmt rekommendera'),
 (9, 6, 5, 475, 'Tråkig book&#13;&#10;                '),
-(10, 6, 1, 279, 'Skriv din recension här...&#13;&#10;                ');
+(10, 6, 1, 279, 'Skriv din recension här...&#13;&#10;                '),
+(11, 7, 5, 600, 'Älskar den boken&#13;&#10;                ');
 
 -- --------------------------------------------------------
 
@@ -122,7 +121,8 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `mobile`) VALUES
 (4, 'Niklas', 'Johnsson', 'n@j.se', '0768646623'),
 (5, 'Anastasia', 'Voronins', 'a@v.se', '0812309712'),
 (6, 'Linda', 'Emilsson', 'l@e.se', '0782354325'),
-(7, 'Georg', 'Jacobsson', 'g@j.se', '0725832254');
+(7, 'Georg', 'Jacobsson', 'g@j.se', '0725832254'),
+(8, 'Elias', 'Hultqvist', 'e@h.se', '075866052');
 
 --
 -- Index för dumpade tabeller
@@ -146,8 +146,8 @@ ALTER TABLE `books`
 --
 ALTER TABLE `userbook`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `bookId` (`book_id`),
-  ADD KEY `userId` (`user_id`);
+  ADD KEY `userbook_ibfk_1` (`book_id`),
+  ADD KEY `userbook_ibfk_2` (`user_id`);
 
 --
 -- Index för tabell `users`
@@ -163,25 +163,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT för tabell `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT för tabell `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT för tabell `userbook`
 --
 ALTER TABLE `userbook`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT för tabell `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Restriktioner för dumpade tabeller
@@ -197,8 +197,8 @@ ALTER TABLE `books`
 -- Restriktioner för tabell `userbook`
 --
 ALTER TABLE `userbook`
-  ADD CONSTRAINT `userbook_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
-  ADD CONSTRAINT `userbook_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `userbook_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `userbook_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
